c6ecff67a92a74bb14871972d16dfae1ca51fef9
kumarsasy <kumarsasy@gmail.com>
2023-12-23 09:18:52 +0530

    uc1 done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,16 @@
+import java.util.Random;
 
 public class EmployeeWage {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Employee Wage Problem");
+
+		Random random = new Random();
+		boolean isPresent = random.nextBoolean();
+		if (isPresent) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
