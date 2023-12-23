e7a3651181119ac739bbd5b4200ef8ef94212a12
kumarsasy <kumarsasy@gmail.com>
2023-12-23 09:26:59 +0530

    uc3 done

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -15,7 +15,14 @@ public class EmployeeWage {
 
 		int WagePerHour = 20;
 		int FullDayHour = 8;
+        int PartTimeHour = 4;
 		int DailyWage = WagePerHour*FullDayHour;
 		System.out.println("Daily employee wage: " + DailyWage);
+		
+		int FullTimeDailyWage = WagePerHour*FullDayHour;
+        System.out.println("Daily full time employee wage: " + FullTimeDailyWage);
+        
+        int PartTimeDailyWage = WagePerHour*PartTimeHour;
+        System.out.println("Daily full time employee wage: " + PartTimeDailyWage);
 	}
 }
